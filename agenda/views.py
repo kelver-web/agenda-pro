5623c153e52c2f29c85167a6e036fb3511099023
kelver <kelverwt@gmail.com>
2021-05-29 16:24:36 -0300

    commit dos arquivos modificados

--- agenda/views.py
+++ agenda/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'agenda/home.html')
+
+
+def login_user(request):
+    return render(request, 'agenda/login.html')
+
+def submit_login(request):
+    if request.method == "POST":
+        username = request.POST.get('username') 
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/home')
+        
+        else:
+            messages.error(request, 'Usuário ou senha inválido!')
+    
+    return redirect('/home')
